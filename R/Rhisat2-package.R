65330e7526589d222601e642c014438409d1203d
Charlotte Soneson <charlottesoneson@gmail.com>
2019-05-17 10:40:09 +0200

    Add Rhisat2-package documentation

---
+++ R/Rhisat2-package.R
@@ -0,0 +1,19 @@
+#' R hisat2 wrapper
+#'
+#' This package provides an R wrapper around the HISAT2 short read aligner. The
+#' package is used by the QuasR Bioconductor package. We recommend to use QuasR
+#' instead of calling Rhisat2 directly, since it covers the whole analysis
+#' workflow of typical high-throughput sequencing experiments.
+#'
+#' @author Charlotte Soneson, based partly on code from Florian Hahne
+#'
+#' @name Rhisat2-package
+#'
+#' @seealso hisat2, hisat2_build, hisat2_build_usage, hisat2_usage
+#'
+#' @references
+#' Kim D, Langmead B and Salzberg SL. HISAT: a fast spliced aligner with low
+#' memory requirements. Nature Methods 12:357-360 (2015).
+#'
+#' @docType package
+NULL
